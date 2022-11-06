fa6f93d1732c2647e4cb524a25e6bf2b24368bef
Grace.Liao <grace.liao@sas.com>
2022-11-06 16:11:09 -0500

    Added styling changes on page components and added test files

--- src/pages/manage-services.jsx
+++ src/pages/manage-services.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import {Stack, Switch} from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 class ManageServices extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Manage Services Page</h2>
-                <Button variant="contained">START FILE WATCHER </Button>
-            </div>
+            <React.Fragment>
+                <CssBaseline/>
+                <Container maxWidth="lg">
+                    <Box sx={{height: '100vh'}}>
+                        <h2>Manage Services</h2>
+                        <Stack direction="row" spacing={1} alignItems="center">
+                            <Typography style={{ paddingRight: '50px' }}>File Watcher Service</Typography>
+                            <Typography>Off</Typography>
+                            {/*<Switch defaultChecked />*/}
+                            <Switch />
+                            <Typography>On</Typography>
+                        </Stack>
+                    </Box>
+                </Container>
+            </React.Fragment>
         )
     }
 }
--- src/pages/upload-files.jsx
+++ src/pages/upload-files.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
 class UploadFiles extends React.Component {
     render() {
         return (
-            <div>
-                <h2>Upload Files Page</h2>
-                <Button variant="contained">UPLOAD </Button>
-            </div>
+            <React.Fragment>
+                <CssBaseline/>
+                <Container maxWidth="lg">
+                    <Box sx={{height: '100vh'}}>
+                        <h2>Upload Files</h2>
+                        <Button variant="contained">UPLOAD </Button>
+                    </Box>
+                </Container>
+            </React.Fragment>
         )
     }
 }

c72453d5935f6b50d8b093bb786ee29cd241dd24
Grace.Liao <grace.liao@sas.com>
2022-11-07 17:14:54 -0500

    start and stop file watcher process working

--- src/api.js
+++ src/api.js
@@ -5,14 +5,17 @@ const FILE_WATCHER_URL = ROOT_URL+'file-watcher'
 
 // FILE WATCHER API
 function startFileWatcher() {
+    // return Promise.resolve({"data": {status: "running", message: null, error: null}})
     return Axios.post(FILE_WATCHER_URL, { "action": "start" })
 }
 
 function stopFileWatcher() {
+    // return Promise.resolve({"data": {status: "stopped", message: null, error: null}})
     return Axios.post(FILE_WATCHER_URL, { "action": "stop" })
 }
 
 function fetchFileWatcherStatus() {
+    // return Promise.resolve({"data": {status: "running", message: null, error: null}})
     return Axios.get(FILE_WATCHER_URL)
 }
 
--- src/pages/manage-services.jsx
+++ src/pages/manage-services.jsx
@@ -9,65 +9,50 @@ import {fetchFileWatcherStatus, startFileWatcher, stopFileWatcher} from "../api"
 class ManageServices extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { checked: false };
     }
 
     componentDidMount() {
         fetchFileWatcherStatus().then((response) => {
             console.log(response)
             this.setState((state, props) => {
-                return {status: response.data["status"]}
+                console.log("inside componentDidMount status is : ", response.data["status"])
+                return {checked: response.data["status"] === "running"}
             });
         })
     }
 
-    onChange = (e) => {
-        const turnOn = e.target.checked
-        if ( turnOn ) {
-            console.log("*** turning ON File Watcher")
+    switchHandler = (e) => {
+        const checked = e.target.checked
+        if ( checked ) {
             startFileWatcher().then((response) => {
                 const data = response.data
 
-                // Handle errors
-                if (data["error"] !== null) {
-                    this.setState(() => {
-                        return {status: this.state.status, dialog: data["message"]}
-                    })
-                }
-
                 if (data["status"] === "running") {
                     this.setState(() => {
-                        return { status: this.state.status, dialog: "File Watcher has been turned ON."}
+                        return { checked: true, dialog: "File Watcher has been turned ON."}
                     })
                 }
 
                 if (data["status"] === "stopped") {
                     this.setState(() => {
-                        return { status: this.state.status, dialog: "File Watcher failed to turn ON."}
+                        return { checked: false, dialog: "File Watcher failed to turn ON."}
                     })
                 }
             })
         } else {
-            console.log("*** turning OFF File Watcher")
             stopFileWatcher().then((response) => {
                 const data = response.data
 
-                // Handle errors
-                if (data["error"] !== null) {
-                    this.setState(() => {
-                        return {status: this.state.status, dialog: data["message"]}
-                    })
-                }
-
                 if (data["status"] === "stopped") {
                     this.setState(() => {
-                        return { status: this.state.status, dialog: "File Watcher has been turned OFF."}
+                        return { checked: false, dialog: "File Watcher has been turned OFF."}
                     })
                 }
 
                 if (data["status"] === "running") {
                     this.setState(() => {
-                        return { status: this.state.status, dialog: "File Watcher failed to turn OFF."}
+                        return { checked: true, dialog: "File Watcher failed to turn OFF."}
                     })
                 }
             }
@@ -88,7 +73,7 @@ class ManageServices extends React.Component {
                         <Stack direction="row" spacing={1} alignItems="center">
                             <Typography style={{ paddingRight: '50px' }}>File Watcher Service</Typography>
                             <Typography>Off</Typography>
-                            <Switch onChange={this.onChange}/>
+                            <Switch checked={this.state.checked} onChange={this.switchHandler}/>
                             <Typography>On</Typography>
                         </Stack>
                         <div> Dialog: { this.state.dialog }</div>
